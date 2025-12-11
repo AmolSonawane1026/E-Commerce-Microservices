052fb65fd4458e6de738b4c566271417b814a283
AmolSonawane1026 <sonawaneamol787@gmail.com>
2025-12-11 10:19:18 +0530

    changed the url of web-client in auth , order, and product service

--- services/order-service/src/index.js
+++ services/order-service/src/index.js
@@ -11,7 +11,7 @@ const app = express();
 // Middleware
 app.use(cors({
   origin: process.env.NODE_ENV === 'production'
-    ? ['https://e-commerce-microservices-auth-servi.vercel.app']
+    ? ['https://e-commerce-web-client.vercel.app']
     : ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:5173'],
   credentials: true
 }));
